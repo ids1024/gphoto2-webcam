49fbf30362a413d1716a9020e763af87f4d1a7f9
Ian Douglas Scott <ian@iandouglasscott.com>
2019-12-30 10:27:12 -0800

    Add code to actually write frames to v4l

--- src/gphoto2-webcam.cpp
+++ src/gphoto2-webcam.cpp
@@ -9,6 +9,22 @@
 const unsigned WIDTH = 960;
 const unsigned HEIGHT = 640;
 
+void setup_v4l(QApplication& app, const char *path, GphotoCamera *camera) {
+    // XXX should be able to pass GphotoCamera as value; results in double free
+    V4l2Loopback loopback(path);
+    loopback.set_vidformat(HEIGHT, WIDTH);
+    auto timer = new QTimer();
+    timer->setInterval(30);
+    app.connect(timer, &QTimer::timeout, &app, [=]() mutable {
+        const char *data;
+        unsigned long int size;
+        GphotoCameraFile file = camera->capture_preview();
+        file.get_data_and_size(&data, &size);
+        loopback.write_frame(data, size);
+    });
+    timer->start();
+}
+
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
 
@@ -18,23 +34,13 @@ int main(int argc, char **argv) {
     GphotoCamera camera(ctx);
     camera.init();
 
+    // XXX path
+    setup_v4l(app, "/dev/video0", &camera);
+
     GphotoCameraWidget config = camera.get_config();
     auto window = *create_qgphoto_widget(config);
 
     window->show();
     window->setWindowTitle("Gphoto2-Webcam");
     return app.exec();
-
-    /*
-    V4l2Loopback loopback("/dev/video0");
-    loopback.set_vidformat(HEIGHT, WIDTH);
-
-    for (;;) {
-        const char *data;
-        unsigned long int size;
-        GphotoCameraFile file = camera.capture_preview();
-        file.get_data_and_size(&data, &size);
-        loopback.write_frame(data, size);
-    }
-    */
 }
